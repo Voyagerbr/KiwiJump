253eafeb3ba2fb1790da9489f3263a15dbd0cf8c
Nicolas <slateste295@gmail.com>
2024-10-31 09:36:16 -0300

    Innitial Commit

---
+++ Kiiwi/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Kiiwi;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Kiiwi/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Kiiwi;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
